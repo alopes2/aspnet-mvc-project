7d65e8b2205fc59bfe0d5445599aaa455c2b1de7
Andre Lopes <andrevitorlopes@gmail.com>
2017-12-17 20:37:44 -0200

    Added customers controller, added table to Index in Movies view and list to movies controller

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -51,15 +51,20 @@ namespace Vidly.Controllers
         }
 
         //movies
-        public ActionResult Index(int? pageIndex, string sortBy)
+        public ViewResult Index(int? pageIndex, string sortBy)
         {
-            if (!pageIndex.HasValue)
-                pageIndex = 1;
+            var movies = GetMovies();
 
-            if (String.IsNullOrWhiteSpace(sortBy))
-                sortBy = "Name";
+            return View(movies);
+        }
 
-            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        private static IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+                    {
+                        new Movie { Id = 1, Name = "Shrek" },
+                        new Movie { Id = 2, Name = "Wall-e" }
+                    };
         }
     }
 }
