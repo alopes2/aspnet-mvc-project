8c11a9b3c85b19ea0fc3c9860772779a2adf9417
Andre Lopes <andrevitorlopes@gmail.com>
2017-12-17 16:50:44 -0200

    Transitioned to attribute routes

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -22,6 +22,7 @@ namespace Vidly.Controllers
             //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
         }
 
+        [Route("movies/released/{year}/{month:regex(\\d{4}):range(1,12)}")]
         public ActionResult ByReleaseDate(int year, int month)
         {
             return Content(year + "/" + month);
