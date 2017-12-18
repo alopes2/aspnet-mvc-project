49ab54e44db6cefcee7f18fc33f21106e834c05d
Andre Lopes <andrevitorlopes@gmail.com>
2017-12-18 21:53:42 -0200

    Added _context to retrieve movies from database and Details action to retrieve one movie

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 using Vidly.Models;
 using Vidly.ViewModel;
 
@@ -10,6 +11,13 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
         // GET: Movies/Random
         public ActionResult Random()
         {
@@ -51,20 +59,26 @@ namespace Vidly.Controllers
         }
 
         //movies
-        public ViewResult Index(int? pageIndex, string sortBy)
+        public ViewResult Index()
         {
-            var movies = GetMovies();
+            var movies = _context.Movies
+                    .Include(m => m.Genre)
+                    .ToList();
 
             return View(movies);
         }
 
-        private static IEnumerable<Movie> GetMovies()
+        //movies/details
+        public ActionResult Details(int id)
         {
-            return new List<Movie>
-                    {
-                        new Movie { Id = 1, Name = "Shrek" },
-                        new Movie { Id = 2, Name = "Wall-e" }
-                    };
+            var movie = _context.Movies
+                    .Include(m => m.Genre)
+                    .SingleOrDefault(m => m.Id == id);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            return View(movie);
         }
     }
 }
