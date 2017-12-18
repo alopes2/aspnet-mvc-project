70138e27eb231fcf27b273225563d56ee9363105
Andre Lopes <andrevitorlopes@gmail.com>
2017-12-18 20:08:41 -0200

    Added Birthdate to Customer

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -10,6 +10,7 @@ namespace Vidly.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public bool IsSubscribedToNewsletter { get; set; }
+        public DateTime? Birthdate { get; set; }
         public MembershipType MembershipType { get; set; }
         public byte MembershipTypeId { get; set; }
     }
