fcddc127ec3482b458829e3593807a77fd5c87bc
Andre Lopes <andrevitorlopes@gmail.com>
2017-12-22 15:33:45 -0200

    Implement Movies front end

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -71,11 +71,11 @@ namespace Vidly.Controllers
         //movies
         public ViewResult Index()
         {
-            var movies = _context.Movies
-                    .Include(m => m.Genre)
-                    .ToList();
+            //var movies = _context.Movies
+            //        .Include(m => m.Genre)
+            //        .ToList();
 
-            return View(movies);
+            return View();
         }
 
         //movies/New
