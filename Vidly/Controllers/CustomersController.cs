dd423e434de17905c4ab49bc58552f34ca9d50c4
Andre Lopes <andrevitorlopes@gmail.com>
2017-12-23 12:11:25 -0200

    Adding cache just for study purposes

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
+using System.Runtime.Caching;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
@@ -73,7 +74,10 @@ namespace Vidly.Controllers
             //var customers = _context.Customers
             //    .Include(c => c.MembershipType)
             //    .ToList();
+            if (MemoryCache.Default["Genres"] == null)
+                MemoryCache.Default["Genres"] = _context.Genres.ToList();
 
+            var genres = MemoryCache.Default["Genres"] as IEnumerable<Genre>;
             return View(/*customers*/);
         }
 
