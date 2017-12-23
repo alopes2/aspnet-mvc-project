139459ba2805ddcf121037cde9e9e3373767822e
Andre Lopes <andrevitorlopes@gmail.com>
2017-12-23 20:04:28 -0200

    Add NumberAvailable

--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -29,5 +29,8 @@ namespace Vidly.Models
         [Display(Name = "Number in Stock")]
         [Range(1,20)]
         public int NumberInStock { get; set; }
+
+        public int NumberAvailable { get; set; }
+
     }
 }
