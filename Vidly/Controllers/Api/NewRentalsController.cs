5b9ce88a97cacb0272d6804580c1f2e80c355079
Andre Lopes <andrevitorlopes@gmail.com>
2017-12-23 20:01:14 -0200

    Implemente CreateNewRentals method

--- Vidly/Controllers/Api/NewRentalsController.cs
+++ Vidly/Controllers/Api/NewRentalsController.cs
@@ -22,7 +22,27 @@ namespace Vidly.Controllers.Api
         [HttpPost]
         public IHttpActionResult CreateNewRentals(NewRentalDto newRental)
         {
-            throw new NotImplementedException();
+            var customer = _context.Customers
+                .Single(c => c.Id == newRental.CustomerId);
+
+            var movies = _context.Movies
+                .Where(m => newRental.MovieIds.Contains(m.Id));
+
+            foreach (var movie in movies)
+            {
+                var rental = new Rental()
+                {
+                    Customer = customer,
+                    Movie = movie,
+                    DateRented = DateTime.Now
+                };
+
+                _context.Rentals.Add(rental);
+            }
+
+            _context.SaveChanges();
+
+            return Ok();
         }
     }
 }
